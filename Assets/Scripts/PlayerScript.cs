5686607fec576d1a942affcf51d191043d3288be
Takeo Watanabe <takeowatanabe3@gmail.com>
2023-02-01 17:38:59 +0100

    zzzz player

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    public Transform planetPivot;
+    bool ground = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        //OnPlanetMovement();
+        OnRootMovement();
+    }
+
+    private void OnPlanetMovement()
+    {
+        planetPivot.Rotate(new Vector3(0, 0, -Input.GetAxisRaw("Horizontal")));
+    }
+
+    private void OnRootMovement()
+    {
+        rb.velocity = new Vector2(0, Input.GetAxisRaw("Vertical"));
+    }
+}
